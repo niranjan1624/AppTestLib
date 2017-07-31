61346320b44a74cfe66d7060dfba0c459a82a2c8
niranjan999 <niranjanp@byteridge.com>
2017-07-31 15:28:09 +0530

    second commit

---
+++ mylibrary/src/main/java/com/byteridge/mylibrary/Niranjan.java
@@ -0,0 +1,14 @@
+package com.byteridge.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Niranjan P on 7/31/2017.
+ */
+
+public class Niranjan {
+    public static void tostMyName(String name, Context context) {
+        Toast.makeText(context, name, Toast.LENGTH_LONG).show();
+    }
+}
